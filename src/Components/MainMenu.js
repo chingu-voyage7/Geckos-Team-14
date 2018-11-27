85876488168a968261782ddd084b570b8b6ea940
Carlos <clockwerkz@gmail.com>
2018-11-26 23:27:00 -0800

    now properly hides menu when close button is clicked.

--- src/Components/MainMenu.js
+++ src/Components/MainMenu.js
@@ -19,25 +19,27 @@ class MainMenu extends React.Component {
 
     render = () => {
         return (
-            <div className={this.state.isOpen ? "main-menu main-menu--show" : "main-menu"}>
-                <div className="main-menu__section">
-                    <h2 className="main-menu__title">Menu</h2>
-                    <button 
-                        className="btn btn--close-menu"
-                        onClick={this.toggleCloseButton}
-                        ><FontAwesomeIcon icon={faTimes}/>
-                    </button>
-                </div>
-                <div className="main-menu__section">
-                    <ul className="options__list">
-                        <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faBrush}/>Change Background</li>
-                        <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faFilter}/>Filter Cards</li>
-                        <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faFill}/>Stickers</li>
-                        <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faEllipsisH}/>More</li>
-                    </ul>
-                </div>
-                <div className="main-menu__section">
-                    <h3 className="activity-menu__title"><FontAwesomeIcon className="icon__item" icon={faTasks}/>Activity</h3>
+            <div className="main-menu__wrapper">
+                <div className={this.state.isOpen ? "main-menu main-menu--show" : "main-menu"}>
+                    <div className="main-menu__section">
+                        <h2 className="main-menu__title">Menu</h2>
+                        <button 
+                            className="btn btn--close-menu"
+                            onClick={this.toggleCloseButton}
+                            ><FontAwesomeIcon icon={faTimes}/>
+                        </button>
+                    </div>
+                    <div className="main-menu__section">
+                        <ul className="options__list">
+                            <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faBrush}/>Change Background</li>
+                            <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faFilter}/>Filter Cards</li>
+                            <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faFill}/>Stickers</li>
+                            <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faEllipsisH}/>More</li>
+                        </ul>
+                    </div>
+                    <div className="main-menu__section">
+                        <h3 className="activity-menu__title"><FontAwesomeIcon className="icon__item" icon={faTasks}/>Activity</h3>
+                    </div>
                 </div>
             </div>
         );
