3cf366de2d374b498a380bf4c3e9d53b9e5260b3
Carlos <clockwerkz@gmail.com>
2018-11-26 23:16:22 -0800

    Adds Activity Section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainMenu />
+        <MainMenu menuState={false}/>
       </div>
     );
   }
--- src/Components/MainMenu.js
+++ src/Components/MainMenu.js
@@ -1,34 +1,47 @@
 import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilter, faFill, faBrush, faEllipsisH, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faFilter, faFill, faBrush, faEllipsisH, faTimes, faTasks } from '@fortawesome/free-solid-svg-icons';
 
 
-const MainMenu = (props) => {
+class MainMenu extends React.Component {
 
-    return (
-        <div className="main-menu main-menu--show">
-            <div className="main-menu__section">
-                <h2 className="main-menu__title">Menu</h2>
-                <button 
-                    className="btn btn--close-menu"
-                    onClick={(e)=>{
-                        e.preventDefault();
-                        console.log("Closes Menu");
-                    }}
-                    ><FontAwesomeIcon icon={faTimes}/>
-                </button>
+    state = {
+        isOpen: true
+    }
+
+    toggleCloseButton = (e) => {
+        e.preventDefault();
+        this.setState((prevState) => ({
+            isOpen : !prevState.isOpen
+        }))
+    }
+
+    render = () => {
+        return (
+            <div className={this.state.isOpen ? "main-menu main-menu--show" : "main-menu"}>
+                <div className="main-menu__section">
+                    <h2 className="main-menu__title">Menu</h2>
+                    <button 
+                        className="btn btn--close-menu"
+                        onClick={this.toggleCloseButton}
+                        ><FontAwesomeIcon icon={faTimes}/>
+                    </button>
+                </div>
+                <div className="main-menu__section">
+                    <ul className="options__list">
+                        <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faBrush}/>Change Background</li>
+                        <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faFilter}/>Filter Cards</li>
+                        <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faFill}/>Stickers</li>
+                        <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faEllipsisH}/>More</li>
+                    </ul>
+                </div>
+                <div className="main-menu__section">
+                    <h3 className="activity-menu__title"><FontAwesomeIcon className="icon__item" icon={faTasks}/>Activity</h3>
+                </div>
             </div>
-            <div className="main-menu__section">
-                <ul className="options__list">
-                    <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faBrush}/>Change Background</li>
-                    <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faFilter}/>Filter Cards</li>
-                    <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faFill}/>Stickers</li>
-                    <li className="option__item"><FontAwesomeIcon className="icon__item" icon={faEllipsisH}/>More</li>
-                </ul>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default MainMenu;
