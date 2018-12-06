1b42c91435f9f48aaf07684d2e87e03def8b7ca2
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-12-06 14:15:08 -0600

    created and added styling to the  buttons and description form

--- src/Component/CardModal/CardModal.js
+++ src/Component/CardModal/CardModal.js
@@ -7,9 +7,24 @@ const CardModal = ({ content, isModalOpen, toggleModal }) => (
                 <h4>{content}</h4>
                 <button onClick={toggleModal}>x</button>
             </div>
-            <div className="card-modal__content-row">
-                <section className="card-modal__content-col double-col"></section>
-                <aside className="card-modal__content-col"></aside>
+            <div className="row">
+                <section className="col double-col">
+                    <div className="descripton">
+                        <h5 className="description__title">Description</h5>
+                        <form className="description__form">
+                            <textarea
+                                rows="5"
+                                className="description__form-textarea"></textarea>
+                        </form>
+                    </div>
+                </section>
+                <aside className="col">
+                    <p>add to card</p>
+                    <button><i className="fa fa-check-square"></i> <span>Checklist</span></button>
+                    <button><i className="fa fa-clock"></i> <span>Due Date</span></button>
+                    <p>actions</p>
+                    <button><i className="fa fa-trash"></i> <span>Delete</span></button>
+                </aside>
             </div>
         </div>
     </div>
