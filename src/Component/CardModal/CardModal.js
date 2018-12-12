91f51a998b0c12e50c8521eaf407c4bb44b0f385
clockwerkz <clockwerkz@gmail.com>
2018-12-11 21:38:41 -0800

    adds airbnb-datepicker Component

--- src/Component/CardModal/CardModal.js
+++ src/Component/CardModal/CardModal.js
@@ -1,33 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import '../CardModal/datepicker.css';
 
-const CardModal = ({ content, isModalOpen, toggleModal }) => (
-    <div className="card-modal" style={{ display: isModalOpen ? 'block' : 'none' }}>
-        <div className="card-modal__content">
-            <div className="card-modal__content-title">
-                <h4>{content}</h4>
-                <button onClick={toggleModal}>x</button>
-            </div>
-            <div className="row">
-                <section className="col double-col">
-                    <div className="descripton">
-                        <h5 className="description__title">Description</h5>
-                        <form className="description__form">
-                            <textarea
-                                rows="5"
-                                className="description__form-textarea"></textarea>
-                        </form>
-                    </div>
-                </section>
-                <aside className="col">
-                    <p>add to card</p>
-                    <button><i className="fa fa-check-square"></i> <span>Checklist</span></button>
-                    <button><i className="fa fa-clock"></i> <span>Due Date</span></button>
-                    <p>actions</p>
-                    <button><i className="fa fa-trash"></i> <span>Delete</span></button>
-                </aside>
+export default class CardModal extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            createdAt : '',
+            calendarFocused: false
+        }
+    }
+
+    onDateChange = (createdAt) => {
+        if (createdAt) {
+            this.setState(()=> ({ createdAt }));
+        }
+    }
+
+    onCalendarFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused : focused }));
+    }
+
+    render () {
+
+        const { content, isModalOpen, toggleModal } = this.props;
+        return (
+            <div className="card-modal" style={{ display: isModalOpen ? 'block' : 'none' }}>
+            <div className="card-modal__content">
+                <div className="card-modal__content-title">
+                    <h4>{content}</h4>
+                    <button onClick={toggleModal}>x</button>
+                </div>
+                <div className="row">
+                    <section className="col double-col">
+                        <div className="descripton">
+                            <h5 className="description__title">Description</h5>
+                            <form className="description__form">
+                                <textarea
+                                    rows="5"
+                                    className="description__form-textarea"></textarea>
+                            </form>
+                        </div>
+                    </section>
+                    <aside className="col">
+                        <p>add to card</p>
+                        <button><i className="fa fa-check-square"></i> <span>Checklist</span></button>
+           
+                        <SingleDatePicker
+                        placeholder={'Due Date'}
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onCalendarFocusChange}
+                        numberOfMonths={1}
+                        />
+                        <p>actions</p>
+                        <button><i className="fa fa-trash"></i> <span>Delete</span></button>
+                    </aside>
+                </div>
             </div>
         </div>
-    </div>
-)
-
-export default CardModal
+        )
+    }
+}
