bf9716ef7bf2bb39d1e695e4297cec621e08f537
clockwerkz <clockwerkz@gmail.com>
2018-12-18 23:47:31 -0800

    converts listItem into a React Component

--- src/Component/Card/ListItem.js
+++ src/Component/Card/ListItem.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ListItem = (props) => {
-    return (
-        <li className="checklist__item">
-           <input 
-                type="checkbox" name="item" 
-                value={props.item} 
-                checked={props.complete} 
-                onChange={()=>props.onToggleCheckBox(props.index)}
-                className="checklist__complete"
-           />
-           <p className="checklist__name">{props.item} <button onClick={()=>props.onDeleteCheckListItem(props.index)}>X</button></p>
-        </li>
-    );
+class ListItem extends Component {
+
+    render() {
+        const {item, complete, onDeleteCheckListItem, onToggleCheckBox, index} = this.props;
+        return (
+            <li className="checklist__item">
+                <input 
+                        type="checkbox" name="item" 
+                        value={item} 
+                        checked={complete} 
+                        onChange={()=>onToggleCheckBox(index)}
+                        className="checklist__complete"
+                />
+                <p className="checklist__name">{item}</p>
+                <button onClick={()=>onDeleteCheckListItem(index)}>X</button>
+            </li>
+        );
+    }
 }
 
 export default ListItem;
