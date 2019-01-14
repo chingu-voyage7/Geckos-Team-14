03fc37a5967dc0e53b4d40303bae24de99197393
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-01-14 13:03:07 -0600

    User can click outside of list menu to close it.

--- src/Component/List/ListMenu.js
+++ src/Component/List/ListMenu.js
@@ -7,13 +7,28 @@ class ListMenu extends Component {
     titleName: ''
   }
 
-  componentDidMount = () => {
-    this.setState({ titleName:this.props.title });
+  UNSAFE_componentWillMount() {
+    document.addEventListener("mousedown", this.handleToggleListMenu, false);
   }
 
-  
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleToggleListMenu, false);
+  }
+
+  handleToggleListMenu = e => {
+    if (this.node.contains(e.target)) {
+      return;
+    }
+    this.props.toggleListMenu();
+  }
+
+  componentDidMount = () => {
+    this.setState({ titleName: this.props.title });
+  }
+
+
   displayCopyList = () => {
-    this.setState({ menuDisplay : 'copyList' });
+    this.setState({ menuDisplay: 'copyList' });
   }
 
   onCopyTitleChange = (titleName) => {
@@ -26,27 +41,27 @@ class ListMenu extends Component {
     switch (this.state.menuDisplay) {
       case 'copyList':
         return (
-          <div className="list-menu">
+          <div className="list-menu" ref={node => this.node = node}>
             <header className="list-menu__header">
               <p className="list-menu__header-title">Copy List</p>
               <button onClick={toggleListMenu} className="list-menu__header-close-btn"><i className="fas fa-times"></i></button>
             </header>
-            <section className="action-list">  
-              <h4 className="copy-list__title">Name</h4> 
+            <section className="action-list">
+              <h4 className="copy-list__title">Name</h4>
               <div className="copy-menu__title-input">
-                <textarea 
-                  onChange={(e)=>{ this.onCopyTitleChange(e.target.value) }}
-                  className="title-input__text" 
-                  value={this.state.titleName}/>
+                <textarea
+                  onChange={(e) => { this.onCopyTitleChange(e.target.value) }}
+                  className="title-input__text"
+                  value={this.state.titleName} />
               </div>
-              <button 
-                onClick={()=>{
+              <button
+                onClick={() => {
                   let title = this.state.titleName || this.props.title;
                   this.props.copyList(listId, title);
-                  toggleListMenu(); 
-                  }}
+                  toggleListMenu();
+                }}
                 className="btn--menu btn--submit btn--copy-memu">
-                  Create List
+                Create List
                 </button>
             </section>
           </div>
@@ -54,7 +69,7 @@ class ListMenu extends Component {
         break;
       default:
         return (
-          <div className="list-menu">
+          <div className="list-menu" ref={node => this.node = node}>
             <header className="list-menu__header">
               <p className="list-menu__header-title">List Actions</p>
               <button onClick={toggleListMenu} className="list-menu__header-close-btn"><i className="fas fa-times"></i></button>
