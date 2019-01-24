10bce815662f5fb3cd596a7ea803b265fa952611
clockwerkz <clockwerkz@gmail.com>
2019-01-23 22:14:57 -0800

    adds sample state data for users new to the project

--- src/App.js
+++ src/App.js
@@ -25,9 +25,17 @@ class App extends Component {
     this.state = {
       styleType: { backgroundImage: `url(${Scene3})` },
       backgroundType: '',
-      cards: {},
-      lists: {},
-      listOrder: []
+      cards: {'sample_card': {
+        id: 'sample_card',
+        content: 'Sample Card'
+      }},
+      lists: {'sample_list':{
+        id: 'sample_list',
+        title: "Sample List",
+        taskIds: ['sample_card']
+        }
+      },
+      listOrder: ['sample_list']
     };
   }
 
@@ -342,7 +350,7 @@ class App extends Component {
     const { lists, cards, listOrder, styleType } = this.state;
     return (
       <div className="App" style={styleType}>
-        <SimpleStorage parent={this} prefix={"TrelloClone"} />
+        <SimpleStorage parent={this} prefix={"Geckos14_Trello_Clone"} />
         <TrelloNav />
         <BoardNav
           handleBackgroundChange={this.handleBackgroundChange}
