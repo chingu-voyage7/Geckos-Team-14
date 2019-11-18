503b8afbd1e427f47acb2a061b86d4eee289acf5
Juan-Garcia1 <juan_garcia_1995@hotmail.com>
2019-11-18 13:20:58 -0600

    used localStorage to persist Redux state

--- src/store.js
+++ src/store.js
@@ -1,14 +1,39 @@
 import { createStore, compose } from "redux";
 import rootReducer from "./reducers";
 
-const initialState = {};
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+
+const persistedState = loadFromLocalStorage();
+
+// const initialState = {};
 
 const store = createStore(
   rootReducer,
-  initialState,
+  // initialState,
+  persistedState,
   compose(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
